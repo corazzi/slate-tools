f0fa71e99a9c8e0430bf45bc9776dc70dc148621
Sacha Corazzi <sacha.corazzi@strawberry.co.uk>
2020-09-16 13:00:09 +0100

    Add babel loader.

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,12 @@ module.exports = {
     rules: [{
       test: /\.js$/,
       exclude: /node_modules/,
-      use: ['babel-loader'],
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: ['es2015'],
+        },
+      }],
     }],
   },
 };
